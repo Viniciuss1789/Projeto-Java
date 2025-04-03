574223706b317399c9a80f16707ace88b7f278f5
aluno <aluno@LAB02-PC18.grupoflamingolab.com>
2025-04-03 19:52:39 -0300

    Oitava Commit

--- FaculdadeFlamingo20251Semestre/src/lista4/E01.java
+++ FaculdadeFlamingo20251Semestre/src/lista4/E01.java
@@ -3,24 +3,23 @@ package lista4;
 import java.util.Scanner;
 
 public class E01 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("O valor inteiro?");
-		
+
 		int imparOuPar = sc.nextInt();
 
-		
 		if (imparOuPar % 2 == 0) {
 			System.out.println("O número é " + imparOuPar + " Par");
-		}else {
+		} else {
 			System.out.println("O número é " + imparOuPar + " Impar");
 		}
-		
+
 		sc.close();
-		
+
 	}
 
 }
---
+++ FaculdadeFlamingo20251Semestre/src/lista4/E03.java
@@ -0,0 +1,25 @@
+package lista4;
+
+import java.util.Scanner;
+
+public class E03 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Sua nota final");
+		
+		int nota = sc.nextInt();
+		
+		if (nota >=60) {
+			System.out.println("Parabens você foi aprovado");
+		}else {
+			System.out.println("Infelizmente você não foi aprovado");
+		}
+		
+		sc.close();
+	}
+	
+
+}
