298db17cbb20e01cfccbc8c96d0eb20ecb37790e
aluno <aluno@LAB02-PC18.grupoflamingolab.com>
2025-05-05 20:52:24 -0300

    23° Commit

--- FaculdadeFlamingo20251Semestre/src/lista05/E04.java
+++ FaculdadeFlamingo20251Semestre/src/lista05/E04.java
@@ -3,14 +3,18 @@ package lista05;
 import java.util.Scanner;
 
 public class E04 {
-
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Digite o ano: ");
+
+		System.out.print("Digite um ano: ");
 		int ano = sc.nextInt();
 
-	}
+		if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+			System.out.println(ano + " é um ano bissexto.");
+		} else {
+			System.out.println(ano + " não é um ano bissexto.");
+		}
 
+		sc.close();
+	}
 }
