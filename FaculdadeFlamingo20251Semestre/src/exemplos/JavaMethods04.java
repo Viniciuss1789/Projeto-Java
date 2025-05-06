5a0a53f10f86f62f626a7e351ac80d9307c873b0
aluno <aluno@LAB02-PC18.grupoflamingolab.com>
2025-05-06 20:57:12 -0300

    29° Commit

--- FaculdadeFlamingo20251Semestre/src/exemplos/JavaMethods04.java
+++ FaculdadeFlamingo20251Semestre/src/exemplos/JavaMethods04.java
@@ -2,13 +2,16 @@ package exemplos;
 
 public class JavaMethods04 {
 	
-	public static void metodo() {
+	public static void exibirComputador(String marcaComputador, String modeloComputador, float precoComputador) {
 		
+		System.out.println("A marca do computador é: " + marcaComputador);
+		System.out.println("O modelo do computador é: " + modeloComputador);
+		System.out.println("O preço do computador é: " + precoComputador);
 	}
 	
 	public static void main(String[] args) {
 		
-		metodo();
+		exibirComputador("AMD" , "5600GT" , 2069.00f);
 	}
 
 }
