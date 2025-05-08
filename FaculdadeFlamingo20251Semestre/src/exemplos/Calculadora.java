5f547fc6e2c35998136bfc27c85d10a32e7bf6ef
aluno <aluno@LAB02-PC18.grupoflamingolab.com>
2025-05-08 20:50:56 -0300

    31° Commit

---
+++ FaculdadeFlamingo20251Semestre/src/exemplos/Calculadora.java
@@ -0,0 +1,25 @@
+package exemplos;
+
+public class Calculadora {
+
+	static int soma(int numero01, int numero02) {
+		return numero01 + numero02;
+	}
+
+	static int subtracao(int numero01, int numero02) {
+		return numero01 - numero02;
+	}
+
+	static int multiplicacao(int numero01, int numero02) {
+		return numero01 * numero02;
+	}
+
+	static int divisap(int numero01, int numero02) {
+		return numero01 / numero02;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+}
